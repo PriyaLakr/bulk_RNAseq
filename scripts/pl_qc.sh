64014a6c3f9bce6dae473f17cd1ccdb12cf401d5
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-05-19 13:36:33 +0530

    Create pl_qc.sh

---
+++ scripts/pl_qc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+input=$1  
+
+cd $input
+mkdir output_dir
+out_dir=$1/output_dir
+
+fastqc -t $2 $input/*.fastq.gz -o $out_dir
+
+cd $out_dir
+
+multiqc .
+
+echo -e "process completed"
