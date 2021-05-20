7bac32f9bc0fe18bee402e67bad701f085271d6c
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-05-20 21:43:49 +0530

    Update pl_trim.sh

--- scripts/pl_trim.sh
+++ scripts/pl_trim.sh
@@ -2,10 +2,17 @@
 
 # usage: bash pl_trim.sh [infile path] [threads]
 
-cd $input_dir
-mkdir trim_out
-trim=$input_dir/trim_out
-
+help(){
+  echo ""
+  echo -e "i (input_dir)      path to the directory containing input files"
+  echo -e "s (infile_suffix)  suffix of input files; eg, for paired-end reads, some_name_R1.fastq.gz some_name_R2.fastq.gz, suffix would be R2.fastq.gz"
+  echo -e "t (num_thread)     number of threads"
+  echo -e "k (keepBothReads)  Type TRUE to keep both reads after trimming"
+  echo -e "a (adapter_file)   name of the adapter file"
+  echo -e "m (min_len)        min read length feature"
+  exit 1
+  
+}
 
 while getopts "i:s:t:o:m:a:l:" opt
   do
@@ -21,9 +28,16 @@ while getopts "i:s:t:o:m:a:l:" opt
      esac
    done
    
-   
+cd $input_dir
+mkdir trim_out
+trim=$input_dir/trim_out
+
+start=$(date +%s)
+
 for infile in *${infile_suffix};
   do name=$(basename $infile ${infile_suffix}); trimmomatic PE -threads $num_thread ${name}${infile_suffix} ${name}${infile_suffix}  $trim/${name}_R1_trim.fastq.gz  $trim/${name}_R1_untrim.fastq.gz  $trim/${name}_R2_trim.fastq.gz  $trim/${name}_R2_untrim.fastq.gz ILLUMINACLIP:${adapter_file}:2:30:10:8:${keepBothReads} MINLEN:${min_len};
 done
 
-echo "Done!"
+end=$(date +%s)
+
+echo -e "\tProcess completed in ( ($end - $start)/60 ) minutes"
