c2dd11ba24ebbe98f354bd556fd2e8da02eb13b2
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-05-20 20:11:38 +0530

    Update pl_trim.sh

--- scripts/pl_trim.sh
+++ scripts/pl_trim.sh
@@ -14,7 +14,7 @@ while getopts "i:s:t:o:m:a:l:" opt
       s ) infile_suffix="$OPTARG" ;;
       t ) num_thread="$OPTARG" ;;
      # o ) output_dir="$OPTARG" ;;
-      m ) mode="$OPTARG" ;;
+      k ) keepBothReads="$OPTARG" ;;
       a ) adapter_file="$OPTARG" ;;
       l ) min_len="$OPTARG" ;;
       \? ) help, exit 1
@@ -23,7 +23,7 @@ while getopts "i:s:t:o:m:a:l:" opt
    
    
 for infile in *${infile_suffix};
-  do name=$(basename $infile ${infile_suffix}); trimmomatic PE -threads $num_thread ${name}${infile_suffix} ${name}${infile_suffix}  $trim/${name}_R1_trim.fastq.gz  $trim/${name}_R1_untrim.fastq.gz  $trim/${name}_R2_trim.fastq.gz  $trim/${name}_R2_untrim.fastq.gz ILLUMINACLIP:${adapter_file}:2:30:10:8:${mode} MINLEN:${min_len};
+  do name=$(basename $infile ${infile_suffix}); trimmomatic PE -threads $num_thread ${name}${infile_suffix} ${name}${infile_suffix}  $trim/${name}_R1_trim.fastq.gz  $trim/${name}_R1_untrim.fastq.gz  $trim/${name}_R2_trim.fastq.gz  $trim/${name}_R2_untrim.fastq.gz ILLUMINACLIP:${adapter_file}:2:30:10:8:${keepBothReads} MINLEN:${min_len};
 done
 
 echo "Done!"
