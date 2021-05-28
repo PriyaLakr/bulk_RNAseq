c1dcb41ef875d28cdc4690fb52f2dcb39146b101
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-05-28 17:21:41 +0530

    Update pl_counts.sh

--- scripts/pl_counts.sh
+++ scripts/pl_counts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-## ===== feature counts =====
+## ===== feature counts ===== 
+## Annotation files should be same for mapping and read counting 
 
 featureCounts -T $num_threads -p -s $strand -F GTF -a $gtf -o $raw_wd/featureCount/counts.txt $raw_wd/star_results/*Coord.out.bam
 
