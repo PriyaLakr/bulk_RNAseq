59fb257897e5fa4e6ab9ae657e84ddeb97aeefc6
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-05-29 17:17:12 +0530

    Update pl_counts.sh

--- scripts/pl_counts.sh
+++ scripts/pl_counts.sh
@@ -6,7 +6,7 @@ mkdir featureCount_out
 ## ===== feature counts ===== 
 ## Annotation files should be same for mapping and read counting 
 
-featureCounts -F GTF -a $gdir/$gtf -s $strand -T $num_threads -p $outdir/*${suffix} -o $outdir/featureCount_out/${expName}_counts
+featureCounts -F GTF -a $gdir/$gtf -s $strand -T $num_threads -p $outdir/*${suffix} -o $outdir/featureCount_out/${expName}_counts 2> log.out
 
 ## Specificy strand is very important for correct output
 ## ===== htseq counts =====
