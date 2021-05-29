8185ff6bd4d50a498b2c6043970ed0bd63ae3bee
PriyaLakra <66781257+PriyaLakr@users.noreply.github.com>
2021-05-30 01:49:33 +0530

    tested

--- scripts/pl_trim.sh
+++ scripts/pl_trim.sh
@@ -22,7 +22,7 @@ while getopts "i:s:t:o:m:a:l:" opt; do
       k ) keepBothReads="$OPTARG" ;;
       a ) adapter_file="$OPTARG" ;;
       l ) min_len="$OPTARG" ;;
-      \? ) help, exit 1 ;;
+      \? ) help; exit 1 ;;
      esac
    done
    
@@ -32,7 +32,7 @@ mkdir -p $input_dir/trim_out
 start=$(date +%s)
 
 for infile in *${infile_suffix};
-  do name=$(basename $infile ${infile_suffix}); trimmomatic PE -threads $num_thread ${name}${infile_suffix} ${name}${infile_suffix}  $input_dir/trim_out/${name}_R1_trim.fastq.gz  $input_dir/trim_out/${name}_R1_untrim.fastq.gz  $input_dir/trim_out/${name}_R2_trim.fastq.gz  $input_dir/trim_out/${name}_R2_untrim.fastq.gz ILLUMINACLIP:${adapter_file}:2:30:10:8:${keepBothReads} MINLEN:${min_len};
+  do name=$(basename $infile ${infile_suffix}); trimmomatic PE -threads $num_thread ${name}${infile_suffix} ${name}${infile_suffix}  $input_dir/trim_out/${name}_R1_trim.fastq.gz  $input_dir/trim_out/${name}_R1_untrim.fastq.gz  $input_dir/trim_out/${name}_R2_trim.fastq.gz  $input_dir/trim_out/${name}_R2_untrim.fastq.gz ILLUMINACLIP:${adapter_file}:2:30:10:8:${keepBothReads} MINLEN:${min_len}
 done
 
 end=$(date +%s)
